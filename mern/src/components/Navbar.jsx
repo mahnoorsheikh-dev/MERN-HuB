b80dde10e4c84d0e7495fdd81cce60a8107e14a6
mahnoorsheikh-dev <mahnoor.3896@gmail.com>
2026-06-11 23:24:31 +0500

    Website structure

--- mern/src/components/Navbar.jsx
+++ mern/src/components/Navbar.jsx
@@ -10,6 +10,52 @@ export default function Navbar() {
     { name: "Projects", href: "/projects" },
     { name: "Topic", href: "/topic" }
   ];
+
+  const TopicsLinks = {
+  Fundamentals: [
+    { name: "Programming Languages", href: "/programming-languages" },
+    { name: "Version Control", href: "/version-control" },
+    { name: "Software Architecture", href: "/software-architecture" },
+    { name: "Agile Methodologies", href: "/agile-methodologies" },
+    { name: "Project Management", href: "/project-management" },
+  ],
+
+  Development: [
+    { name: "Web Development", href: "/web-development" },
+    { name: "Mobile Development", href: "/mobile-development" },
+    { name: "Game Development", href: "/game-development" },
+    { name: "API Development", href: "/api-development" },
+    { name: "Database Management", href: "/database-management" },
+    { name: "Software Testing", href: "/software-testing" },
+  ],
+
+  DataAndAI: [
+    { name: "Data Science", href: "/data-science" },
+    { name: "Machine Learning", href: "/machine-learning" },
+    { name: "Artificial Intelligence", href: "/artificial-intelligence" },
+    { name: "Big Data", href: "/big-data" },
+  ],
+
+  AdvancedTech: [
+    { name: "Cloud Computing", href: "/cloud-computing" },
+    { name: "Cybersecurity", href: "/cybersecurity" },
+    { name: "DevOps", href: "/devops" },
+    { name: "Containerization", href: "/containerization" },
+    { name: "Microservices", href: "/microservices" },
+    { name: "Serverless Architecture", href: "/serverless-architecture" },
+    { name: "Blockchain", href: "/blockchain" },
+    { name: "Internet of Things", href: "/internet-of-things" },
+    { name: "Virtual Reality", href: "/virtual-reality" },
+    { name: "Augmented Reality", href: "/augmented-reality" },
+    { name: "Quantum Computing", href: "/quantum-computing" },
+  ],
+
+  Design: [
+    { name: "UI/UX Design", href: "/ui-ux-design" },
+    { name: "Software Development Tools", href: "/software-development-tools" },
+  ]
+};
+
   
   return (
     <nav className="bg-[#1E1E2F] text-[#E0E0E0] px-8 py-5 border-b border-[#00BFA6] flex items-center">
