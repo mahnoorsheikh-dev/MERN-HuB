f75f42da8a8e0185130b637fcbbaef5bbe7f0560
mahnoorsheikh-dev <mahnoor.3896@gmail.com>
2026-06-22 17:44:06 +0500

    Linking footer links

--- mern/src/components/Footer.jsx
+++ mern/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import footerLinks from "./footerLinks";
 export default function Footer() {
   return (
     <>
-      <footerLinks />
+      <FooterLinks />
       <footer className="bg-[#1E1E2F] text-[#E0E0E0] py-4 text-center border-t border-[#2f3030]">
         <p className="text-sm text-[#888888]">
           &copy; 2026 MERN Hub. All rights reserved.
